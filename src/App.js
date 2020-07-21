c25affab9dca47ae5d097797cd6d59a4dc810693
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-07-21 10:48:12 -0400

    changed Router to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Footer from './components/Footer';
@@ -10,14 +10,14 @@ import AboutMeContact from './pages/AboutMeContact';
 function App() {
     return (
       <div className="App">
-        <Router >
+        <HashRouter basename={process.env.PUBLIC_URL}>
           <Header />
           <Footer />
           <Route exact path="/" component={AboutMeContact} />
           <Route exact path="/about" component={AboutMeContact} />
           <Route exact path="/portfolio" component={Portfolio} />
           {/* <Portfolio /> */}
-        </Router>
+        </HashRouter>
         {/* <Title>Portfolio of Projects</Title>
         {this.state.projects.map(project => (
           <Portfolio
