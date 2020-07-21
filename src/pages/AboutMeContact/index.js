32b6e24c9864e9cff53957b57466999890d9d6a2
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-07-21 15:42:22 -0400

    small details for clarity in aboutme section

--- src/pages/AboutMeContact/index.js
+++ src/pages/AboutMeContact/index.js
@@ -9,17 +9,17 @@ function AboutMe(props) {
             <div className='col-md-6 background'>
                 <h2 className='bkgd'>Information about Josh Benjamin</h2>
 
-                <li className='card c1'><strong>***In-training through the University of Pennsylvania/Trilogy Education Services</strong> to be a full-stack (front-end/back-end) computer coder</li>
+                <li className='card c1'>***In-training through the University of Pennsylvania/Trilogy Education Services to be a <strong>full-stack (front-end/back-end) computer coder</strong></li>
                 <li className='card c2'><strong>***</strong>Certification to be received <strong>August 2020</strong></li>
                 <li className='card c3'><strong>***
                             Born and raised</strong> in the Bronx, NYC, <strong>living and working</strong> in Philly-/Lancaster, PA-areas</li>
-                <li className='card c4'><strong>***</strong>Career thus far as a <strong>secondary education instructor and higher education</strong></li>
+                <li className='card c4'><strong>***</strong>Career thus far as a <strong>secondary education instructor and higher education</strong>instructor</li>
                 <li className='card c5'><strong>***
-                            Undergrad degrees</strong>: Advanced Linguistics/ESL/Spanish</li>
+                            Undergrad degrees</strong> in Advanced Linguistics/ESL/Spanish</li>
                 <li className='card c6'><strong>***
-                            Master's degree</strong>: TESL (Teaching English to Non-Native English Speakers)</li>
+                            Master's degree</strong> in TESL (Teaching English to Non-Native English Speakers)</li>
                 <li className='card c7'><strong>***
-                            Freelance artist</strong>, focused on Graffiti and Street Murals</li>
+                            Freelance artist,</strong> focused on Graffiti and Street Murals</li>
             </div>
 
             <div className='col-md-6 contact'>
