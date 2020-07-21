b76f77e44ad8522385c168e5b3a4ac4a9b1e9cda
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-07-21 15:38:30 -0400

    added some front page coloring for style

--- src/pages/AboutMeContact/index.js
+++ src/pages/AboutMeContact/index.js
@@ -6,19 +6,19 @@ function AboutMe(props) {
     return (
         <div className='row'>
 
-            <div className='col-md-6'>
-                <h2>Background Information about Josh Benjamin</h2>
+            <div className='col-md-6 background'>
+                <h2 className='bkgd'>Information about Josh Benjamin</h2>
 
-                <li className='card'><strong>***In-training through the University of Pennsylvania/Trilogy Education Services</strong> to be a full-stack (front-end/back-end) computer coder</li>
-                <li className='card'><strong>***</strong>Certification to be received <strong>August 2020</strong></li>
-                <li className='card'><strong>***
+                <li className='card c1'><strong>***In-training through the University of Pennsylvania/Trilogy Education Services</strong> to be a full-stack (front-end/back-end) computer coder</li>
+                <li className='card c2'><strong>***</strong>Certification to be received <strong>August 2020</strong></li>
+                <li className='card c3'><strong>***
                             Born and raised</strong> in the Bronx, NYC, <strong>living and working</strong> in Philly-/Lancaster, PA-areas</li>
-                <li className='card'><strong>***</strong>Career thus far as a <strong>secondary education instructor and higher education</strong></li>
-                <li className='card'><strong>***
+                <li className='card c4'><strong>***</strong>Career thus far as a <strong>secondary education instructor and higher education</strong></li>
+                <li className='card c5'><strong>***
                             Undergrad degrees</strong>: Advanced Linguistics/ESL/Spanish</li>
-                <li className='card'><strong>***
+                <li className='card c6'><strong>***
                             Master's degree</strong>: TESL (Teaching English to Non-Native English Speakers)</li>
-                <li className='card'><strong>***
+                <li className='card c7'><strong>***
                             Freelance artist</strong>, focused on Graffiti and Street Murals</li>
             </div>
 
@@ -29,9 +29,10 @@ function AboutMe(props) {
                 </div>
                 <h2>Contact Me:</h2>
                 <div className='card'>
-                    <li><strong>Email:</strong>joshuasevendavid@gmail.com</li>
-                    <li><strong>Phone:</strong>717.538.0866</li>
-                    <li>See my <strong>Résumé <a href="../documents/JoshuaBenjamin.miscCV.ALT.2020.pdf">here</a></strong></li>
+                    <li className='l1'><strong>Email:</strong>joshuasevendavid@gmail.com</li>
+                    <li className='l2'><strong>Phone:</strong>717.538.0866</li>
+                    <li className='l3'>See my <strong>Résumé <a href="../documents/JoshuaBenjamin.miscCV.ALT.2020.pdf">here</a></strong></li>
+                    <li className='l4'>See my <strong>Github Page <a href="https://github.com/jbenj-upenn">here</a></strong></li>
                 </div>
                 <br></br>
                 <br></br>
